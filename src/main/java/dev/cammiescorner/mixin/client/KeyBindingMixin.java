381f0504c779547b237dc308e4e3a4d5bc075238
CammiePone <inazuma261@gmail.com>
2024-10-21 18:41:26 -0700

    permitted some keybinds

--- src/main/java/dev/cammiescorner/mixin/client/KeyBindingMixin.java
+++ src/main/java/dev/cammiescorner/mixin/client/KeyBindingMixin.java
@@ -5,6 +5,7 @@ import dev.cammiescorner.common.components.TransformationComponent;
 import dev.cammiescorner.common.entities.BeastEntity;
 import dev.cammiescorner.common.registries.ModComponents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,9 +13,12 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.Set;
+
 @Mixin(KeyBinding.class)
 public class KeyBindingMixin {
 	@Unique private final MinecraftClient client = MinecraftClient.getInstance();
+	@Unique private final KeyBinding self = (KeyBinding) (Object) this;
 
 	@Shadow private boolean pressed;
 
@@ -22,7 +26,7 @@ public class KeyBindingMixin {
 	private boolean isNotPressedWhileTransformed(boolean original) {
 		PlayerEntity player = client.player;
 
-		if(!client.isPaused() && player != null && player.getWorld() != null) {
+		if(!client.isPaused() && player != null && player.getWorld() != null && !permittedKeys().contains(self)) {
 			TransformationComponent component = player.getComponent(ModComponents.TRANSFORMATION);
 
 			if(component.isTransformed() && client.getCameraEntity() instanceof BeastEntity) {
@@ -38,7 +42,7 @@ public class KeyBindingMixin {
 	private boolean wasNotPressedWhileTransformed(boolean original) {
 		PlayerEntity player = client.player;
 
-		if(!client.isPaused() && player != null && player.getWorld() != null) {
+		if(!client.isPaused() && player != null && player.getWorld() != null && !permittedKeys().contains(self)) {
 			TransformationComponent component = player.getComponent(ModComponents.TRANSFORMATION);
 
 			if(component.isTransformed() && client.getCameraEntity() instanceof BeastEntity) {
@@ -48,5 +52,21 @@ public class KeyBindingMixin {
 		}
 
 		return original;
+	}
+
+	@Unique
+	private Set<KeyBinding> permittedKeys() {
+		GameOptions options = client.options;
+
+		return Set.of(
+				options.togglePerspectiveKey,
+				options.fullscreenKey,
+				options.commandKey,
+				options.chatKey,
+				options.screenshotKey,
+				options.playerListKey,
+				options.smoothCameraKey,
+				options.socialInteractionsKey
+		);
 	}
 }
